d07ad49199e5801cbfcfafa37048a341849dd616
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-12-16 10:25:55 -0300

    Adicionando classe produto

---
+++ src/model/Produto.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.Objects;
+
+public class Produto {
+
+    private int codigo;
+    private String descricao;
+    private double preco;
+
+    public Produto(int codigo, String descricao, double preco) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return codigo == produto.codigo && Double.compare(preco, produto.preco) == 0 && Objects.equals(descricao, produto.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, descricao, preco);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
